cca09f1ef9d17c255161348cb42be900ca57b33f
bing yang <bingyang9258@gmail.com>
2022-02-16 15:32:28 -0500

    add authorization

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :friends
   #get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "home#index"
+  # root "home#index"
+  root "friends#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_16_050352) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_16_162840) do
   create_table "friends", force: :cascade do |t|
     t.string "frist_name"
     t.string "last_name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_16_050352) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
