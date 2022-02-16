baf774a5fc38512291615df9af89caf8ea7af8d0
bing yang <bingyang9258@gmail.com>
2022-02-15 23:48:33 -0500

    add styles and initial forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :friends
   #get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "home/about"
-  root "home#index"
+  root "friends#index"
 end
