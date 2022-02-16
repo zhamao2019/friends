329f938e067b0e2e630321e1aa97afc973c8e821
bing yang <bingyang9258@gmail.com>
2022-02-16 11:10:14 -0500

    add and style devise for user management

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :friends
   #get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "home/about"
-  root "friends#index"
+  root "home#index"
 end
